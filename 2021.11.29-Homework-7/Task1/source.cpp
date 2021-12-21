85105c15a55591ac992a753be26b40849552bb43
dm17r11 <90861954+dm17r11@users.noreply.github.com>
2021-12-22 01:43:57 +0600

    Update source.cpp

--- 2021.11.29-Homework-7/Task1/source.cpp
+++ 2021.11.29-Homework-7/Task1/source.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char** argv)
 {
-	//print_circle(3);
-	//std::cout << count_steps(13) << "\n";
+	print_circle(3);
+	std::cout << count_steps(13) << "\n";
 
 	return EXIT_SUCCESS;
 }
